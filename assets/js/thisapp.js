4cf8e12fe3d74b9bfbb41be8155e834b4dd4933f
Ronnie Taylor <ronnie@ronnietaylor.com>
2013-06-27 14:00:59 -0700

    added social icons

--- assets/js/thisapp.js
+++ assets/js/thisapp.js
@@ -1,3 +1,12 @@
 myApp = {};
 
 $(document).foundation();
+
+$(document).ready(function() {
+
+  $('.portfolio-item').mouseenter(function(){
+    $(this).find('.portfolio-deets').fadeIn();
+  }).mouseleave(function() {
+    $(this).find('.portfolio-deets').fadeOut();
+  });
+});
