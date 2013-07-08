b516307228638b9debaf159e47997bf1553ff38d
Ronnie Taylor <ronnie@ronnietaylor.com>
2013-07-07 21:10:50 -0700

    lots of styling

--- assets/js/thisapp.js
+++ assets/js/thisapp.js
@@ -1,4 +1,12 @@
-myApp = {};
+// Custom JS app business here
+var aRamos = {};
+
+// Mimic media queries with JS so we can do smart stuff
+if (document.documentElement.clientWidth <= 768){
+	//alert('below 768');
+}
+
+// Basic Jquery bits below here
 
 $(document).foundation();
 
