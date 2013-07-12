f8f6525f3986e5500bd23ed8c775bafac06256db
Ronnie Taylor <ronnie@ronnietaylor.com>
2013-07-12 10:43:02 -0700

    global abatement

--- assets/js/thisapp.js
+++ assets/js/thisapp.js
@@ -10,7 +10,7 @@ var aRamos = {};
 $(document).foundation();
 
 // Prepare to display random client quotes on home page.
-this.randomtip = function(){
+aRamos.randomtip = function(){
   var length = $("#tips li").length;
   var ran = Math.floor(Math.random()*length) + 1;
   $("#tips li:nth-child(" + ran + ")").show();
@@ -19,7 +19,7 @@ this.randomtip = function(){
 $(document).ready(function() {
 
   // Display random client quote on home page
-  randomtip();
+  aRamos.randomtip();
 
 	// Add a hover effect to portfolio items.
   $('.portfolio-item').mouseenter(function(){
